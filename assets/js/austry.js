9049a8c53eea36477f09e6f11d75f56d59cd64c5
abhinand9772 <100460106+abhinand9772@users.noreply.github.com>
2023-07-25 10:56:12 +0530

    updt

--- assets/js/austry.js
+++ assets/js/austry.js
@@ -1001,3 +1001,19 @@
 
 
 })(jQuery);
+
+
+
+
+
+let form = document.getElementById("comment-form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let full_name = document.getElementById("full-name").value;
+  let email = document.getElementById("email").value;
+  let comments = document.getElementById("comments").value;
+
+  window.location.href = `mailto:saiedhadevelopers@gmail.com?subject=Enquiry from ${full_name} ${email}&body= ${comments}`;
+
+  contact();
+});
